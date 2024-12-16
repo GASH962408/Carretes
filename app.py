48dc93eb780118a4d48d7b171029f3707e813eb2
GASH962408 <tu_correo@example.com>
2024-12-16 15:00:26 -0500

    Estilos css

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 from carrete import buscar_carrete  # Importa la función buscar_carrete
+from data import obtener_datos  # Importa la función para obtener los piezómetros
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     resultados = []
+    # Obtener los piezómetros
+    datos = obtener_datos()  # Obtiene los datos de los piezómetros y carretes
+    piezometros = datos["piezometros"]  # Extrae los piezómetros del diccionario
+
     if request.method == "POST":
         piezometro_id = int(request.form["piezometro_id"])
         cable_requerido = int(request.form["cable_requerido"])
@@ -14,7 +19,7 @@ def index():
         # Llamar a la función buscar_carrete para obtener los resultados
         resultados = buscar_carrete(piezometro_id, cable_requerido, costo_por_metro)
 
-    return render_template("home.html", resultados=resultados)
+    return render_template("home.html", resultados=resultados, piezometros=piezometros)
 
 if __name__ == "__main__":
     app.run(debug=True)
